20ff083762feff4a30d49726469ffafb8b1d3456
OnealWrng <wengwork1230@gmail.com>
2024-01-16 19:51:26 +0800

    add second char

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,20 @@
 var dataPoints = Array.from({length: 10}, (_, i) => {
     var date = new Date();
     date.setMinutes(date.getMinutes() - (10 - i));
-    return { x: date, y: Math.random() * 1000 };
+    return { x: date, y: 0 };
   });
+
+var dataPoints2 = Array.from({length: 10}, (_, i) => {
+    var date = new Date();
+    date.setMinutes(date.getMinutes() - (10 - i));
+    return { x: date, y: 0 };
+  });
+
 window.onload = function () {
     var chart = new CanvasJS.Chart("chartContainer",
     {
       title:{
-      text: "Co2"
+      text: "Humidity"
       },
       axisX: {
         interval: 1,
@@ -38,5 +45,39 @@ window.onload = function () {
         // Update the chart
         chart.render();
       }, 60000); // Update every minute
+
+      var chart2 = new CanvasJS.Chart("chartContainer2",
+      {
+        title:{
+        text: "Temperature"
+        },
+        axisX: {
+          interval: 1,
+          intervalType: "minute",
+          valueFormatString: "HH:mm"
+        },
+         data: [
+        {
+          type: "line",
+          xValueType: "dateTime",
+          dataPoints: dataPoints2
+        }
+        ]
+      });
+      chart2.render();
+      setInterval(function () {
+        // Get the current date and time
+        var currentDate = new Date();
+    
+        // Add a new data point
+        dataPoints2.push({ x: currentDate, y: Math.random()  });
+    
+        // Remove the oldest data point if there are more than 12 data points
+        if (dataPoints2.length > 12) {
+          dataPoints2.shift();
+        }
+    
+        // Update the chart
+        chart2.render();
+    }, 60000); // Update every minute
     }
-  
